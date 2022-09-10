b9cc81094a75cf5c230ca9c425384e5e0a00b8f9
Elif Can <elif.can@avkaryazilim.com.tr>
2022-09-10 21:23:43 +0300

    Recursive şekilde asal sayı bulma programı kodları yazıldı.

---
+++ AsalSayi_recursive/src/Main.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int number, result;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Lütfen bir işlem seçiniz :");
+        number = input.nextInt();
+
+        System.out.print(isPrime(number,2) ? number + " sayısı asaldır." : number + " sayısı asal değildir.");
+
+    }
+    static boolean isPrime(int number,int i){
+        if(i+1==number)
+            return true;
+        if(number==2)
+            return true;
+        else if (number<2) {
+            return false;
+        }
+        else {
+            if((number%i)==0)
+                return false;//asal değil
+            else
+                return isPrime(number,i+1);
+        }
+    }
+}
+
