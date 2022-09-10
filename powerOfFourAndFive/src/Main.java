9de21f495d233044d001fe7ca322144a60f4748f
Elif Can <elif.can@avkaryazilim.com.tr>
2022-09-10 16:44:48 +0300

    Girilen sayıya kadar olan 4 ve 5'in kuvvetlerini ekrana yazdıran program

---
+++ powerOfFourAndFive/src/Main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int number, total = 0;
+        String four,five;
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Lütfen bir sayı giriniz : ");
+        number = input.nextInt();
+
+        System.out.println("Dördün kuvvetleri :  ");
+        for(int i=1;i<=number;i*=4){
+            System.out.print(i + " ");
+        }
+        System.out.println();
+        System.out.println("Beşin kuvvetleri :  ");
+        for(int i=1;i<=number;i*=5){
+            System.out.print(i + " ");
+        }
+    }
+}
