9c49a6bc5aaaa972210cac022f94cb03a4273545
Elif Can <elif.can@avkaryazilim.com.tr>
2022-09-10 17:11:00 +0300

    Üslü sayı kodları yazıldı.

--- powerOfFourAndFive/src/Main.java
+++ powerOfFourAndFive/src/Main.java
@@ -3,7 +3,6 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         int number, total = 0;
-        String four,five;
         Scanner input = new Scanner(System.in);
 
         System.out.print("Lütfen bir sayı giriniz : ");
